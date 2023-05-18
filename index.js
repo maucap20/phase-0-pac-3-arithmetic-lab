82f0d0a34464ff61e6f612cb02b92125464ae370
Adam Mauch <maucap20@wfu.edu>
2023-05-18 08:51:44 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,31 @@
+const { directive } = require("babel-types")
+
+function add(a, b) {
+    return a+b;
+}
+function subtract(a, b) {
+    return a-b;
+
+}
+function multiply(a, b) {
+    return a*b;
+
+}
+function divide(a, b) {
+    return a/b;
+
+}
+function increment(n) {
+    n++;
+    return n;
+}
+function decrement(n) {
+    n--;
+    return n;
+}
+function makeInt(string) {
+    return parseInt(string, 10);
+}
+function preserveDecimal(string) {
+    return parseFloat(string);
+}
